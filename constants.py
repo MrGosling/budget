e462353c1b0b76ac54b84580a702472138677743
Marx <relictpriest@gmail.com>
2025-06-20 19:39:24 +0300

    reuse loading constants from env

--- constants.py
+++ constants.py
@@ -1,3 +1,10 @@
-DB_NAME = 'budget_db'
-MONGO_URI = 'mongodb://localhost:27017'
-COLLECTION_NAME = 'expenses'
+import os
+
+from utils import load_environ
+
+load_environ()
+
+DB_NAME = os.getenv(key='DB_NAME')
+MONGO_URI = os.getenv(key='MONGO_URI')
+COLLECTION_NAME = os.getenv(key='COLLECTION_NAME')
+
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 from pymongo import MongoClient
 from constants import MONGO_URI, DB_NAME, COLLECTION_NAME
 
@@ -12,3 +15,23 @@ def get_mongo_collection():
     client = MongoClient(MONGO_URI)
     db = client[DB_NAME]
     return db[COLLECTION_NAME]
+
+
+def load_environ() -> None:
+    """
+    Загружает переменные окружения из файла .env в текущей директории.
+
+    Файл должен содержать строки в формате KEY=VALUE.
+    Каждая переменная будет добавлена в os.environ.
+
+    Исключения:
+        Если файл .env не найден, выводится сообщение 'File not found!'.
+    """
+    try:
+        env_path = Path('.') / '.env'
+        with open(env_path) as file:
+            for line in file:
+                key, value = line.strip().split('=')
+                os.environ[key] = value
+    except FileNotFoundError:
+        print('File not found!')
